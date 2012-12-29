a8e8794a6ab0202a4ab438fa16d4fcf07e2f0886
cbmeeks <cbmeeks@gmail.com>
2012-12-29 12:32:11 -0500

    added JADE support so that I can include handlebars.js later

--- server.js
+++ server.js
@@ -3,27 +3,27 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express	= require('express')
+,	routes	= require('./routes')
+,	http	= require('http')
+,	path	= require('path');
 
 var app = express();
 
+
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('O;AI%!Xl(zC8`DAX4m.9~h-nmN<kdch]6@_AWUX78XJ+8a%U>gTmA.kGqdJ1Lt2i6BFJWaERrVpS5NQH6w35cYdBVThhY2NItnovx0fxXm6lT8sp9Eq4jxSShI'));
+	app.use(express.session());
+	app.use(app.router);
+	app.use(require('less-middleware')({ src: __dirname + '/public' }));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
@@ -31,7 +31,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
